2c2e6492f0a217dd1834cfb6a9e918b928e1fbcd
MiniSowgro <132415164+MiniSowgro@users.noreply.github.com>
2024-10-17 20:50:47 +0800

    Update Main.js
    dont worry the us will give you a pass

--- about/Main.js
+++ about/Main.js
@@ -1,12 +1,20 @@
-function openNav() {
-	console.log("Opening Sidebar.")
-	document.getElementById("mySidebar").style.width = "250px";
-	document.querySelector(".openbtn").style.left = "270px";
+var menuButton = document.getElementById("menu");
+var sidebar = document.querySelector(".sidebar");
+
+var sidebarOpen = false;
+
+function toggleNav() {
+    if (sidebarOpen === false) {
+        sidebar.style.width = "250px";
+        menuButton.style.left = "270px";
+
+        sidebarOpen = true;
+    } else {
+        sidebar.style.width = "0";
+        menuButton.style.left = "10px";
+
+        sidebarOpen = false;
+    }
 }
 
-function closeNav() {
-	console.log("Closing Sidebar.")
-	document.getElementById("mySidebar").style.width = "0";
-	document.querySelector(".openbtn").style.left = "10px";
-}
-
+menuButton.addEventListener('click', toggleNav)
